0c772f0b4259811763893759ac8ec9fd2296f66d
Christopher Grote <christopher.grote@uk.ibm.com>
2017-05-17 14:39:55 +0100

    Moved to simple in/out nodes that are multi-modal in and of themselves

--- ibm-igc.js
+++ ibm-igc.js
@@ -43,90 +43,122 @@ module.exports = function(RED) {
   });
 
   /**
-   * Run a search, using the received payload as the query (or, if not an object, use the 
-   * query string defined within the node itself)
+   * Retrieve metadata from IGC based on the retrieval option selected
    */
-  function IGCSearchNode(n) {
+  function IGCInNode(n) {
     RED.nodes.createNode(this, n);
 
     this.server = RED.nodes.getNode(n.server);
-    const node = this;
-    if (!this.server) {
-      node.error("No IGC configuration node present", {});
-    }
+    this.search = n.search;
+    this.query  = n.query;
+    this.url    = n.url;
+    this.rid    = n.rid;
+    this.ridtype = n.ridtype;
+    this.ridproperties = n.ridproperties;
 
-    this.query = n.query;
-
-    node.on("input", function(msg) {
-
-      const receivedQ = (typeof msg.payload === "object") ? msg.payload : this.query;
-      igcrest.search(receivedQ, function(err, result) {
-        _sendResultsOnPayload(node, err, result);
-      });
-
-    });
-
-  }
-  RED.nodes.registerType("IGC search", IGCSearchNode);
-
-  /**
-   * Get metadata using any arbitrary GET-based REST URL (e.g. next page of results from 'next', full asset details from '_url', etc)
-   */
-  function IGCGetURL(n) {
-    RED.nodes.createNode(this, n);
-
-    this.server = RED.nodes.getNode(n.server);
     const node = this;
     if (!this.server) {
       node.error("No IGC configuration node present", {});
     }
 
     node.on("input", function(msg) {
-      const receivedURL = (typeof msg.url === "string") ? msg.url : "";
-      let callURL = receivedURL;
-      if (receivedURL.indexOf('https://') !== -1) {
-        callURL = receivedURL.substring(receivedURL.indexOf('/ibm/iis/igc-rest'));
-      }
-      igcrest.makeRequest('GET', callURL, null, null, null, function(res, resSearch) {
-        let err = null;
-        if (res.statusCode !== 200) {
-          err = "Unsuccessful request " + res.statusCode + "\n" + res.headers;
-          node.error(err);
-          node.error('headers: ', res.headers);
-        }
-        _sendResultsOnPayload(node, err, resSearch);
-      });
-    });
 
-  }
-  RED.nodes.registerType("IGC get", IGCGetURL);
+      if (node.search === "_query_") {
 
-  /**
-   * Update metadata by RID and update JSON
-   */
-  function IGCUpdate(n) {
-    RED.nodes.createNode(this, n);
+        const receivedQ = (typeof msg.query === "object") ? msg.query : this.query;
+        igcrest.search(receivedQ, function(err, result) {
+          _sendResultsOnPayload(node, err, result);
+        });
 
-    this.server = RED.nodes.getNode(n.server);
-    const node = this;
-    if (!this.server) {
-      node.error("No IGC configuration node present", {});
-    }
+      } else if (node.search === "_id_") {
 
-    node.on("input", function(msg) {
-      const receivedRID = (typeof msg.rid === "string") ? msg.rid : "";
-      const updateJSON  = (typeof msg.update === "object") ? msg.update : {};
-      igcrest.update(receivedRID, updateJSON, function(err, result) {
-        if (err) {
-          node.error(err, err);
+        const rid        = (typeof msg.rid === "string") ? msg.rid : this.rid;
+        const properties = Array.isArray(msg.properties) ? msg.properties : this.ridproperties.split(",");
+
+        if (properties.length === 0) {
+          igcrest.getAssetById(rid, function(err, result) {
+            _sendResultsOnPayload(node, err, result);
+          });
         } else {
-          node.log("Update result: " + result);
+          const type = (typeof msg.type === "string") ? msg.type : this.ridtype;
+          igcrest.getAssetPropertiesById(rid, type, properties, 10, true, function(err, result) {
+            _sendResultsOnPayload(node, err, result);
+          });
         }
-      });
+
+      } else if (node.search === "_url_") {
+
+        const receivedURL = (typeof msg.url === "string") ? msg.url : this.url;
+        let callURL = receivedURL;
+        if (receivedURL.indexOf('https://') !== -1) {
+          callURL = receivedURL.substring(receivedURL.indexOf('/ibm/iis/igc-rest'));
+        }
+        igcrest.makeRequest('GET', callURL, null, null, null, function(res, resSearch) {
+          let err = null;
+          if (res.statusCode !== 200) {
+            err = "Unsuccessful request " + res.statusCode + "\n" + res.headers;
+            node.error(err);
+            node.error('headers: ', res.headers);
+          }
+          _sendResultsOnPayload(node, err, resSearch);
+        });
+
+      }
+
     });
 
   }
-  RED.nodes.registerType("IGC update", IGCUpdate);
+  RED.nodes.registerType("IGC in", IGCInNode);
+
+  /**
+   * Retrieve metadata from IGC based on the retrieval option selected
+   */
+  function IGCOutNode(n) {
+    RED.nodes.createNode(this, n);
+
+    this.server = RED.nodes.getNode(n.server);
+
+    this.operation = n.operation;
+    this.rid       = n.rid;
+    this.assettype = n.assettype;
+    this.details   = n.details;
+
+    const node = this;
+    if (!this.server) {
+      node.error("No IGC configuration node present", {});
+    }
+
+    node.on("input", function(msg) {
+
+      if (node.operation === "create") {
+
+        const type        = (typeof msg.type === "string") ? msg.type : this.assettype;
+        const detailsJSON = (typeof msg.details === "object") ? msg.details : {};
+        igcrest.create(type, detailsJSON, function(err, result) {
+          _logResultsOnCompletion(node, err, "Create result", result);
+        });
+
+      } else if (node.search === "update") {
+
+        const receivedRID = (typeof msg.rid === "string") ? msg.rid : "";
+        const detailsJSON = (typeof msg.details === "object") ? msg.details : {};
+        igcrest.update(receivedRID, detailsJSON, function(err, result) {
+          _logResultsOnCompletion(node, err, "Update result", result);
+        });
+
+      } else if (node.search === "delete") {
+
+        const rid = (typeof msg.rid === "string") ? msg.rid : this.rid;
+        igcrest.deleteAssetById(rid, function(err, result) {
+          _logResultsOnCompletion(node, err, "Delete result", result);
+        });
+
+      }
+
+    });
+
+  }
+  RED.nodes.registerType("IGC out", IGCOutNode);
 
   function _sendResultsOnPayload(node, err, result) {
     let results = null;
@@ -136,6 +168,14 @@ module.exports = function(RED) {
       node.error(err, err);
     }
     node.send(results);
+  }
+
+  function _logResultsOnCompletion(node, err, msg, result) {
+    if (err) {
+      node.error(err, err);
+    } else {
+      node.log(msg + ": " + result);
+    }
   }
 
 };
